c7672f1fed683839ec92e43a28c18f107976675b
CamiloSalamanca88 <camilo.i.salamanca@gmail.com>
2026-06-23 03:39:51 -0400

    Habilitar JSR250 para roles en admin

--- src/main/java/com/vademecum/security/SecurityConfig.java
+++ src/main/java/com/vademecum/security/SecurityConfig.java
@@ -14,9 +14,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
